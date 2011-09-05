393b06b9178641e8ea35a550c60bb35f337da626
ming <ming4883@gmail.com>
2011-09-05 13:03:30 +0800

    add filter options to crCreateFBOTexture2D

--- webcl/crender.js
+++ webcl/crender.js
@@ -225,8 +225,8 @@ function crCreateFBOTexture2D(width, height, options) {
 	tex.type = options.type || gl.UNSIGNED_BYTE;
 	
 	gl.bindTexture(gl.TEXTURE_2D, tex);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, options.mag_filter || gl.NEAREST);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, options.min_filter || gl.NEAREST);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 	
